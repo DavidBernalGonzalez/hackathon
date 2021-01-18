5e363532cee57831aabb7f826ed7f61099ffe178
David <eldeividlh@gmail.com>
2021-01-18 12:35:25 +0100

    Exer02 DONE

--- exer02/main.js
+++ exer02/main.js
@@ -1,27 +1,12 @@
-lastId = 0;
-element = '';
-
-setInterval(() => {
-    removeClass("target");
-    const id = Math.floor(Math.random() * 9) + 1;
-    this.lastId = id;
-    const element = document.getElementById(id);
-    element.classList.add("target");
-    element.addEventListener("click", printMsg);
-    this.element = element;
-}, 3000);
-
-function removeClass(className) {
-    try {
-        if (document.getElementsByClassName(className)) {
-            const faqToggle = document.getElementsByClassName(className)[0];
-            if (faqToggle.classList.contains(className)) {
-                faqToggle.classList.remove(className);
-            }
-            element.removeEventListener("click", printMsg);
-        }
-    } catch (err) { }
-}
-
-let printMsg = () => alert("Has ganado");
-
+document.addEventListener("keypress", function (e) {
+    let color = '';
+    if (e.key.toLowerCase() === 'w') {
+        color = 'white';
+    } else if (e.key.toLowerCase() === 'y') {
+        color = 'yellow';
+    } else if (e.key.toLowerCase() === 'r') {
+        color = 'red';
+    }
+    document.getElementById("tests").className = '';
+    document.getElementById("tests").classList.add(color);
+});
