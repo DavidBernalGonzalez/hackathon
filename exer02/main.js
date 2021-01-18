76f5017dcc8f40acaba855cc5ac4f70e31a587da
David <eldeividlh@gmail.com>
2021-01-18 12:14:55 +0100

    Exer1 DONE

---
+++ exer02/main.js
@@ -0,0 +1,27 @@
+lastId = 0;
+element = '';
+
+setInterval(() => {
+    removeClass("target");
+    const id = Math.floor(Math.random() * 9) + 1;
+    this.lastId = id;
+    const element = document.getElementById(id);
+    element.classList.add("target");
+    element.addEventListener("click", printMsg);
+    this.element = element;
+}, 3000);
+
+function removeClass(className) {
+    try {
+        if (document.getElementsByClassName(className)) {
+            const faqToggle = document.getElementsByClassName(className)[0];
+            if (faqToggle.classList.contains(className)) {
+                faqToggle.classList.remove(className);
+            }
+            element.removeEventListener("click", printMsg);
+        }
+    } catch (err) { }
+}
+
+let printMsg = () => alert("Has ganado");
+
